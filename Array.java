ba0bd2b0a7c505ee1d963215c948db9818a98344
Olawale <taofeekolawale159@gmail.com>
2026-05-11 11:57:27 +0100

    New Task

---
+++ Array.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class DuplicateElimination{
+
+    public static void main(String args[]){
+
+    Scanner userInput = new Scanner(System.in);
+
+    int [] array = new int [10];
+
+    for(int count = 0; count < array.length; count++)
+}
+}
